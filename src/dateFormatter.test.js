fb533ab21b79f4f9d324910f1b9a4a4d7f234e20
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-09 20:42:21 +0000

    Update test to expect a formatted timestamp back

--- src/dateFormatter.test.js
+++ src/dateFormatter.test.js
@@ -2,6 +2,8 @@ import dateFormatter from './dateFormatter';
 
 describe('dateFormatter', () => {
   it('should return timestamp', () => {
-    expect(dateFormatter('123')).toEqual('123');
+    expect(dateFormatter('2016-09-11T21:18:00Z')).toEqual('2016/09/11 21:18');
+    expect(dateFormatter('2016-03-17T16:30:19Z')).toEqual('2016/03/17 16:30');
+    expect(dateFormatter('2016-02-26T10:05:17Z')).toEqual('2016/02/26 10:05');
   })
 });

c1e78d9e86ab5fc6b99ef57691175918f60ea30a
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-09 20:55:04 +0000

    Update Message module test to use the date formatter

--- src/Message.test.js
+++ src/Message.test.js
@@ -1,6 +1,13 @@
+jest.mock('./dateFormatter');
+
 import React from 'react';
 import { shallow } from 'enzyme';
 import Message from './Message';
+import dateFormatter from './dateFormatter';
+
+const mockDateValue = '9/11/2016, 10:18:00 PM';
+
+dateFormatter.mockReturnValue(mockDateValue);
 
 const defaultProps = {
   "messageId": "1469e146-075f-46d2-888b-8d3152da1be9",
@@ -21,7 +28,7 @@ describe('Message', () => {
     const wrapper = shallow(<Message {...defaultProps} />);
 
     expect(wrapper.find('img')).toHaveLength(1);
-    expect(wrapper.find('.message-timestamp').props().children).toContain(defaultProps.timestamp);
+    expect(wrapper.find('.message-timestamp').props().children).toContain(mockDateValue);
     expect(wrapper.find('.message-email').props().children).toContain(defaultProps.email);
     expect(wrapper.find('.message-name').props().children).toContain(defaultProps.fullName);
     expect(wrapper.find('.message-text').props().children).toContain(defaultProps.message);
@@ -30,6 +37,12 @@ describe('Message', () => {
   it('renders avatar when avatar is a string', () => {
     const wrapper = shallow(<Message {...defaultProps} avatar={null} />);
     expect(wrapper.find('img')).toHaveLength(0);
+  });
+
+  it('call the dateFormatter', () => {
+    const testDate = '2016-02-03T04:05:06Z';
+    const wrapper = shallow(<Message {...defaultProps} timestamp={testDate} />);
+    expect(dateFormatter).toHaveBeenCalledWith(testDate);
   });
 
 });

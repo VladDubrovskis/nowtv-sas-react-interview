6f7080529a482c08dff774312118f9baf32b9cab
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-09 18:58:05 +0000

    Add array sorting by date to fix the test

--- src/service.js
+++ src/service.js
@@ -3,18 +3,22 @@ import { getMessages, getMembers } from './data';
 export default async function getChatLog() {
   const messages = await getMessages();
   const members = await getMembers();
-  return Promise.resolve(messages.map((message) => {
-    const memberDetails = members.find((member) => member.id === message.userId);
+  return Promise.resolve(
+    messages
+      .map((message) => {
+        const memberDetails = members.find((member) => member.id === message.userId);
 
-    return {
-      avatar: memberDetails.avatar,
-      email: memberDetails.email,
-      fullName: `${memberDetails.firstName} ${memberDetails.lastName}`,
-      message: message.message,
-      timestamp: message.timestamp,
-      userId: memberDetails.id,
-      messageId: message.id
-    }
-  }));
+        return {
+          avatar: memberDetails.avatar,
+          email: memberDetails.email,
+          fullName: `${memberDetails.firstName} ${memberDetails.lastName}`,
+          message: message.message,
+          timestamp: message.timestamp,
+          userId: memberDetails.id,
+          messageId: message.id
+        }
+      })
+      .sort((message1, message2) => (new Date(message1.timestamp) - new Date(message2.timestamp)))
+  );
 
 };

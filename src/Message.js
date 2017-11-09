a68e6dd1ed283c201c43978c415ffce1afe72178
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-09 20:57:07 +0000

    Update Message module to include the date formatter

--- src/Message.js
+++ src/Message.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import dateFormatter from './dateFormatter';
 
 const Message = ({ avatar, email, timestamp, message, fullName }) => {
   return (
       <div className="message">
         <div className="message-avatar">{avatar ? <img src={avatar} /> : ''}</div>
-        <div className="message-timestamp">{ timestamp }</div>
+        <div className="message-timestamp">{ dateFormatter(timestamp) }</div>
         <div className="message-email">{ email }</div>
         <div className="message-name">{ fullName }</div>
         <div className="message-text">{ message }</div>

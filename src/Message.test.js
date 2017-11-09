3541380e0ac5f96c462cbc18aa2839064acc8c25
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-09 20:10:56 +0000

    Update the Message component test to include an image when avatar is available

--- src/Message.test.js
+++ src/Message.test.js
@@ -6,4 +6,9 @@ describe('Message', () => {
   it('renders without crashing', () => {
     shallow(<Message />);
   });
+
+  it('renders avatar when avatar is a string', () => {
+    const wrapper = shallow(<Message avatar={"http://test"} />);
+    expect(wrapper.find('img')).toHaveLength(1);
+  });
 });

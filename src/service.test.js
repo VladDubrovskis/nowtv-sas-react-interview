d7a54cc99329c4bca87f71c7a06bd0bbfd0da517
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-09 18:31:15 +0000

    Update the service test: mock out data sources and update the expectation

--- src/service.test.js
+++ src/service.test.js
@@ -1,15 +1,89 @@
+jest.mock('./data');
+
+import { getMembers, getMessages } from './data';
+
+getMembers.mockReturnValue(Promise.resolve([
+  {
+    "id": "cff77726-363e-4ba6-a953-4dc431cdd383",
+    "firstName": "Ralph",
+    "lastName": "Nguyen",
+    "email": "rnguyen6@odnoklassniki.ru",
+    "avatar": "http://dummyimage.com/100x100.png/dddddd/000000",
+    "ip": "192.86.3.21"
+  },
+  {
+    "id": "0bdd3633-d683-432d-add1-4c0d4f841ec3",
+    "firstName": "Lois",
+    "lastName": "Wheeler",
+    "email": "lwheeler7@harvard.edu",
+    "avatar": null,
+    "ip": "131.205.120.5"
+  },
+  {
+    "id": "c980934a-f54e-459e-8137-0ec3654cdafb",
+    "firstName": "Aaron",
+    "lastName": "Williams",
+    "email": "awilliams8@indiegogo.com",
+    "avatar": "http://dummyimage.com/100x100.jpg/dddddd/000000",
+    "ip": "88.59.162.204"
+  }
+]));
+
+getMessages.mockReturnValue(Promise.resolve([
+  {
+    "id": "1469e146-075f-46d2-888b-8d3152da1be9",
+    "userId": "cff77726-363e-4ba6-a953-4dc431cdd383",
+    "message": "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet.",
+    "timestamp": "2016-08-21T02:55:32Z"
+  },
+  {
+    "id": "75ebf4b8-a63b-48d3-b348-b67e6e898b55",
+    "userId": "0bdd3633-d683-432d-add1-4c0d4f841ec3",
+    "message": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.",
+    "timestamp": "2016-03-17T16:30:19Z"
+  },
+  {
+    "id": "555f4e1b-90a6-4079-850d-54001f29ce44",
+    "userId": "c980934a-f54e-459e-8137-0ec3654cdafb",
+    "message": "Quisque ut erat. Curabitur gravida nisi at nibh.",
+    "timestamp": "2016-02-26T10:05:17Z"
+  }
+]));
+
 import getChatLog from './service';
 
-// This is just a sample test to show you the desired format and fields, feel free to delete it.
-
-it('returns the correct format', () => {
-  return getChatLog().then(([firstMessage]) => {
-    expect(typeof firstMessage.messageId).toBe('string');
-    expect(typeof firstMessage.userId).toBe('string');
-    expect(typeof firstMessage.fullName).toBe('string');
-    expect(typeof firstMessage.timestamp).toBe('string');
-    expect(typeof firstMessage.email).toBe('string');
-    expect(typeof firstMessage.message).toBe('string');
-    expect(firstMessage.avatar === null || typeof firstMessage === 'string').toBeTruthy();
+it('returns the correct format back in the right order sorted by time', () => {
+  return getChatLog().then((result) => {
+    expect(result).toEqual([
+      {
+        "messageId": "1469e146-075f-46d2-888b-8d3152da1be9",
+        "userId": "cff77726-363e-4ba6-a953-4dc431cdd383",
+        "fullName": "Ralph Nguyen",
+        "timestamp": "2016-08-21T02:55:32Z",
+        "email": "rnguyen6@odnoklassniki.ru",
+        "message": "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet.",
+        "avatar": "http://dummyimage.com/100x100.png/dddddd/000000",
+      },
+      {
+        "messageId": "75ebf4b8-a63b-48d3-b348-b67e6e898b55",
+        "userId": "0bdd3633-d683-432d-add1-4c0d4f841ec3",
+        "fullName": "Lois Wheeler",
+        "timestamp": "2016-03-17T16:30:19Z",
+        "email": "lwheeler7@harvard.edu",
+        "message": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.",
+        "avatar": null
+      },
+      {
+        "messageId": "12356",
+        "userId": "c980934a-f54e-459e-8137-0ec3654cdafb",
+        "fullName": "Aaron Williams",
+        "timestamp": "2016-02-26T10:05:17Z",
+        "email": "awilliams8@indiegogo.com",
+        "message": "Quisque ut erat. Curabitur gravida nisi at nibh.",
+        "avatar": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+      },
+    ]);
   });
 });
+
+
